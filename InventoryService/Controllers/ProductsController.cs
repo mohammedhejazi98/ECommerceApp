8c920418f10ad6cdb7baf268372c20d525f38116
mohammedhejazi98 <64383814+mohammedhejazi98@users.noreply.github.com>
2024-11-18 16:33:35 +0300

    Remove WeatherForecast, add ProductsController.
    Deleted WeatherForecast-related classes and controllers from InventoryService and OrderService. Introduced a new ProductsController in the InventoryService to handle product-related API requests.

---
+++ InventoryService/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using InventoryService.Data;
+using InventoryService.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(DataContext dataContext) : ControllerBase
+    {
+        #region Public Methods
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            var products =  dataContext.Products.AsEnumerable();
+            return products;
+        }
+
+        #endregion Public Methods
+    }
+}
